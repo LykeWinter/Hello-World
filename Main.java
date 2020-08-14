eecea8c896ff555d3ca720dd1b947e613ef86cb1
LykeWinter <lykeWinter@gmail.com>
2020-08-14 11:10:58 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String []args) {
+		System.out.println("Hello world");
+		System.out.println("My name is Kim");
+		System.out.println("I reading fantasy books");
+		System.out.println("I like playing league");
+		System.out.println("I like tech stuff");
+	}
+}
